bae31d85841e6dd80719d455a7544d40394ab442
joaovsbraz <joaovictor3.14@outlook.com>
2024-07-10 17:00:56 -0300

    feat: add email list invite feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, X } from 'lucide-react';
-import { useState } from 'react';
+import { MapPin, Calendar, ArrowRight, UserRoundPlus, Settings2, X, AtSign, Plus } from 'lucide-react';
+import { FormEvent, useState } from 'react';
 
 export function App() {
   const [isGuestsInputVisible, setIsGuestsInputVisible] = useState(false);
   const [isGuestsModalVisible, setIsGuestsModalVisible] = useState(false);
+  const [emailList, setEmailList] = useState(['joao.braz@gmail.com']);
 
   const handleGuestsInput = () => setIsGuestsInputVisible(!isGuestsInputVisible);
   const handleGuestsModal = () => setIsGuestsModalVisible(!isGuestsModalVisible);
+  const handleEmailList = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const data = new FormData(e.currentTarget);
+    const email = data.get('email')?.toString();
+
+    if (!email) return;
+
+    if(emailList.includes(email)) return;
+
+    setEmailList([...emailList, email]);
+
+    e.currentTarget.reset();
+  }
+  const handleRemoveEmailFromEmailList = (emailToRemove : string) => {
+    const newEmailList = emailList.filter(email => email != emailToRemove);
+    setEmailList(newEmailList);
+  }
 
   return (
     <div className="h-screen flex items-center justify-center bg-pattern bg-no-repeat bg-center">
@@ -73,19 +92,42 @@ export function App() {
 
       {isGuestsModalVisible && (
         <div className='fixed inset-0 bg-black/60 flex items-center justify-center'>
-
-          <div className='w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900'>
-
-            <div className='flex items-center justify-between'>
-              <h2 className='text-lg font-semibold'>Selecionar convidados</h2>
-              <button onClick={handleGuestsModal}>
-                <X className='size-5 text-zinc-400' />
-              </button>
+          <div className='w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5'>
+            <div className='space-y-2'>
+              <div className='flex items-center justify-between'>
+                <h2 className='text-lg font-semibold'>Selecionar convidados</h2>
+                <button onClick={handleGuestsModal}>
+                  <X className='size-5 text-zinc-400' />
+                </button>
+              </div>
+              <p className='text-sm text-zinc-400'>Os convidados irão receber e-mails para confirmar a participação na viagem.</p>
             </div>
-            <p>Os convidados irão receber e-mails para confirmar a participação na viagem.</p>
-            
+            <div className='flex flex-wrap gap-2'>
+              {
+                emailList.map((email) => {
+                  return (
+                    <div key={email} className='py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2'>
+                      <span className='text-zinc-300'>{email}</span>
+                      <button>
+                        <X className='size-4 text-zinc-400' onClick={() => handleRemoveEmailFromEmailList(email)}/>
+                      </button>
+                    </div>
+                  )
+                })
+              }
+            </div>
+            <div className="w-full h-px bg-zinc-800" />
+            <form onSubmit={handleEmailList} className='p-2.5 bg-zinc-950 border-l-zinc-800 rounded-lg flex items-center gap-2'>
+              <div className='px-2 flex items-center flex-1 gap-2'>
+                <AtSign className='text-zinc-400 size-5' />
+                <input type="email" name="email" id="" placeholder="Digite o e-mail do convidado" className="bg-transparent text-lg placeholder-zinc-400 w-32 outline-none flex-1" />
+              </div>
+              <button type='submit' className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
+                Confirmar viagem
+                <Plus className='size-5' />
+              </button>
+            </form>
           </div>
-
         </div>
       )}
 
